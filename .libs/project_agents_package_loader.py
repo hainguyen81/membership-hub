27badce11a4d13c970b2df23864808029d8658af
hainguyen81 <hainguyen81@users.noreply.github.com>
2026-07-23 23:02:30 +0700

    - update libs

--- .libs/project_agents_package_loader.py
+++ .libs/project_agents_package_loader.py
@@ -14,3 +14,14 @@ if phys_agents_package_path.is_dir():
 
 else:
     print(f"⛔ Not define enviroment 'AGENTS_PACKAGE_PATH' to register package/module finder")
+
+# load project workspace as python package
+project_workspace_path = os.environ.get('PROJECT_WORKSPACE')
+phys_project_workspace_path = Path(project_workspace_path).resolve()
+if phys_project_workspace_path.is_dir():
+    projectWorkspacePackageFinder = FolderPackageFinder(str(phys_project_workspace_path));
+    sys.meta_path.insert(0, projectWorkspacePackageFinder)
+    print(f"✅ Registered {project_workspace_path} to sys.meta_path for finding with alias: {projectWorkspacePackageFinder.alias()}")
+
+else:
+    print(f"⛔ Not define enviroment 'AGENTS_PACKAGE_PATH' to register package/module finder")
