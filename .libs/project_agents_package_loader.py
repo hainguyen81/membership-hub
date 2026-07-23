51f9607446bddceadbf864233ead9bc0822980d9
hainguyen81 <hainguyen81@users.noreply.github.com>
2026-07-23 22:55:40 +0700

    - update libs

--- .libs/project_agents_package_loader.py
+++ .libs/project_agents_package_loader.py
@@ -1,13 +1,14 @@
 import os
-import importlib
+import sys
 from pathlib import Path
 
 from modules import FolderPackageFinder
 
 # load current folder as python package
 agents_package_path = os.environ.get('AGENTS_PACKAGE_PATH')
-if agents_package_path and len(agents_package_path) > 0:
-    agentsPackageFinder = FolderPackageFinder(agents_package_path);
+phys_agents_package_path = Path(agents_package_path).resolve()
+if phys_agents_package_path.is_dir():
+    agentsPackageFinder = FolderPackageFinder(str(phys_agents_package_path));
     sys.meta_path.insert(0, agentsPackageFinder)
     print(f"✅ Registered {agents_package_path} to sys.meta_path for finding with alias: {agentsPackageFinder.alias()}")
 
