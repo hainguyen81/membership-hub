10a442a02e47e03a01da027f62b8fce02af9e018
hainguyen81 <hainguyen81@users.noreply.github.com>
2026-07-23 20:56:25 +0700

    - update libs

--- .libs/project_agents_package_loader.py
+++ .libs/project_agents_package_loader.py
@@ -10,11 +10,13 @@ def load_project_agents_package():
     agents_folder = Path(AGENTS_PACKAGE_PATH).resolve()
     if agents_folder.is_dir():
         # load enviroment agents package
-        agents_package = load_folder_as_package(str(agents_folder))
+        return load_folder_as_package(str(agents_folder))
     
     else:
         print(f"❌ Not found enviroment 'AGENTS_PACKAGE_PATH' or its value is not an existing folder to load...")
+        return None
 
 # load agents package
 if __name__ == "__main__":
-    importlib.import_module(load_project_agents_package())
+    agents_package = load_project_agents_package()
+    importlib.import_module(agents_package) if agents_package
